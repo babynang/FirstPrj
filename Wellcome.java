f9dae5f099647ab3a47c84a0ca6a6c3eea6d6a1a
babynang <danghongty2012@gmail.com>
2024-08-12 14:34:58 +0700

    first commit

---
+++ Wellcome.java
@@ -0,0 +1,6 @@
+public class Wellcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Dang Hong Ty!");
+        System.out.println("Wellcome to the DHKTPM19B");
+    }
+}
